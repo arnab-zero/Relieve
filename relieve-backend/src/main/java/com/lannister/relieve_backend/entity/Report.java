2cf65445e0a0c8de87497f90e22ab3aead16e679
arnab-zero <darnabhere@gmail.com>
2024-09-15 09:32:36 +0600

    Relieve client setup done

---
+++ relieve-backend/src/main/java/com/lannister/relieve_backend/entity/Report.java
@@ -0,0 +1,5 @@
+package com.lannister.relieve_backend.entity;
+
+public class Report {
+    
+}
---
+++ relieve-client/src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className="text-2xl">Sorry! Can not access this page.</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
